aa2227eab0f875d19d5d5ab7807e6634f1573741
dayuallen <dayuallen@gmail.com>
2016-10-12 22:19:38 -0400

    this is the branch for sidenav

--- components/stars.ctrl.js
+++ components/stars.ctrl.js
@@ -4,12 +4,21 @@
 
 	angular
 		.module('ngStars')
-		.controller('starsCtrl', function($scope, $http, starsFactory){
+		.controller('starsCtrl', function($scope, $http, starsFactory, $mdSidenav){
 
 			starsFactory.getStars().then(function(response){
 				$scope.stars = response.data;
 
 			});
+
+			$scope.openSidebar = function(){
+				$mdSidenav('left').open();//becuase our md-component-id is left
+			}
+
+			$scope.closeSidebar = function(){
+				$mdSidenav('left').close();
+			}
+
 		});
 
 })();

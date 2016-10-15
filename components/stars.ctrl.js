2f273e0e9068fe61979e481470faf64d603c261c
dayuallen <dayuallen@gmail.com>
2016-10-14 22:32:03 -0400

    add new star to ngStar

--- components/stars.ctrl.js
+++ components/stars.ctrl.js
@@ -4,12 +4,18 @@
 
 	angular
 		.module('ngStars')
-		.controller('starsCtrl', function($scope, $http, starsFactory, $mdSidenav){
+		.controller('starsCtrl', function($scope, $http, starsFactory, $mdSidenav, $mdToast){
 
 			starsFactory.getStars().then(function(response){
 				$scope.stars = response.data;
 
 			});
+
+			var contact = {
+				name: "Allen",
+				phone: "(555)555-5555",
+				email:"starTech@gmail.com"
+			}
 
 			$scope.openSidebar = function(){
 				$mdSidenav('left').open();//becuase our md-component-id is left
@@ -17,6 +23,21 @@
 
 			$scope.closeSidebar = function(){
 				$mdSidenav('left').close();
+			}
+
+			$scope.saveStar = function(star){
+				if (star) {
+					star.contact = contact;
+					$scope.stars.push(star);
+					$scope.star = {};
+					$scope.closeSidebar();
+					$mdToast.show(
+						$mdToast.simple()
+							.content("Star saved!")
+							.position('top, right')
+							.hideDelay(3000)
+					);
+				}
 			}
 
 		});
